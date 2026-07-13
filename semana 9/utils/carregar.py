62a45a820905b6c0a8c2a6f3cd65a3b83d84ef6a
freddybarnajunior <freddybarna3@gmail.com>
2026-07-12 21:40:05 -0300

    Update carregar.py

--- semana 9/utils/carregar.py
+++ semana 9/utils/carregar.py
@@ -1,1 +1,11 @@
-
+def carregar_dados():
+    """Lê os arquivos PDAD, filtra sentinelas e realiza o merge."""
+    moradores = pd.read_csv('moradores.csv', sep=';')
+    domicilios = pd.read_excel('domicilios.xlsx')
+    
+    # Filtragem de valores sentinela (99999 e 88888)
+    moradores = moradores[(moradores['escolaridade'] != 99999) & (moradores['escolaridade'] != 88888)]
+    
+    # Merge das tabelas
+    df = pd.merge(moradores, domicilios, on='A01nficha', suffixes=('_m', '_d'))
+    return df
